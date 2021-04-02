d248206b355c4963f7df7abaa83cdda818f36f11
monthalcantara <montival_junior@yahoo.com.br>
2021-04-01 23:31:15 -0300

    Estudo estrutura de dados com arrays

--- src/main/java/io/github/monthalcantara/estudos/estrutradedados/alura/array/Vetor.java
+++ src/main/java/io/github/monthalcantara/estudos/estrutradedados/alura/array/Vetor.java
@@ -9,28 +9,76 @@ public class Vetor {
     private Aluno[] alunos = new Aluno[100];
     private int totalDeAlunos = 0;
 
+
     public void adiciona(Aluno aluno) {
+        garanteEspaco();
         alunos[totalDeAlunos] = aluno;
         totalDeAlunos++;
     }
 
+    /*
+        A ideia saqui é adicionar um elemento numa posição do array e mover os existentes para
+        que o mesmo seja adicionado e não sobreponha os existentes
+    */
+    public void adiciona(int posicao, Aluno aluno) {
+        garanteEspaco();
+        if (!posicaoValida(posicao)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        //  Com "totalDeAlunos - 1" eu tenho o valor do indice do ultimo componente do array
+        //  Vou iterando e contando menos um a cada laço até que i seja menor que a posição informada
+        // "i >= posicao" por que eu tenho que mover pra frente todos os lementos depois da posição...
+        // ...inclusive o da posição para a entrada no novoque que desejo incluir
+        for (int i = this.totalDeAlunos - 1; i >= posicao; i--) {
+            // Na posição atual mais 1, ou seja, na proxima posição eu coloco o elemento da posição atual
+            this.alunos[i + 1] = this.alunos[i];
+        }
+        //Adiciono na posição desejada
+        this.alunos[posicao] = aluno;
+        // Atualizo a contagem de elementos no array
+        this.totalDeAlunos++;
+    }
+
     public Aluno pega(int posicao) {
         if (posicaoOcupada(posicao)) {
-            return alunos[posicao];
+            return this.alunos[posicao];
         }
-       throw new IllegalArgumentException("Posição inválida");
+        throw new IllegalArgumentException("Posição inválida");
     }
 
     // Quero saber se nessa posção possui algo diferente de null
     private boolean posicaoOcupada(int posicao) {
-        return totalDeAlunos > posicao && posicao >= 0;
+        return this.totalDeAlunos > posicao && posicao >= 0;
     }
+
     private boolean posicaoValida(int posicao) {
-        return totalDeAlunos >= posicao && posicao >= 0;
+        return this.totalDeAlunos >= posicao && posicao >= 0;
+    }
+
+    /*
+    Verifica se a lista está cheia alunos.length == totalDeAlunos
+    caso esteja, dobra o tamanho da lista
+     */
+    public void garanteEspaco() {
+        if (alunos.length == totalDeAlunos) {
+            Aluno[] novaLista = new Aluno[alunos.length * 2];
+            for (int i = 0; i < alunos.length; i++) {
+                novaLista[i] = alunos[i];
+            }
+            alunos = novaLista;
+        }
     }
 
     public void remove(int posicao) {
 
+        if (!posicaoOcupada(posicao)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        for (int i = posicao; i <= this.totalDeAlunos - 1; i++) {
+            alunos[i] = alunos[i + 1];
+        }
+        this.totalDeAlunos--;
     }
     /*
     Esse tipo de código gera o O(N) ou seja eu preciso varrer toda a
--- src/main/java/io/github/monthalcantara/estudos/estrutradedados/alura/array/VetorTeste.java
+++ src/main/java/io/github/monthalcantara/estudos/estrutradedados/alura/array/VetorTeste.java
@@ -6,24 +6,39 @@ public class VetorTeste {
 
     public static void main(String[] args) {
 
-    Aluno a1 = new Aluno("José");
-    Aluno a2 = new Aluno("João");
+        Aluno a1 = new Aluno("José");
+        Aluno a2 = new Aluno("João");
+        Aluno a3 = new Aluno("Maria");
 
-    Vetor lista = new Vetor();
+        Vetor lista = new Vetor();
 
-    System.out.println(lista.tamanho());
+        System.out.println(lista.tamanho());
 
-    lista.adiciona(a1);
+        lista.adiciona(a1);
 
-    System.out.println(lista.tamanho());
+        System.out.println(lista.tamanho());
 
-    lista.adiciona(a2);
+        System.out.println(lista);
 
-    System.out.println(lista.tamanho());
+        lista.adiciona(a2);
 
-    System.out.println(lista);
+        System.out.println(lista.tamanho());
 
-    System.out.println(lista.contem(a1));
+        System.out.println(lista);
+
+        lista.adiciona(1, a3);
+
+        System.out.println(lista.tamanho());
+
+        System.out.println(lista);
+
+        System.out.println(lista.contem(a1));
+
+        lista.remove(1);
+
+        System.out.println(lista.tamanho());
+
+        System.out.println(lista);
 
         System.out.println(lista.pega(1));
 
